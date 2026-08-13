7f46b324f07868703b67148c810d8282b0d94cec
sranger <yyh65668794@gmail.com>
2026-08-13 21:24:23 +0800

    Align Solo12 training with Go2 baseline

--- legged_gym/legged_gym/envs/solo12/solo12_v3_1_upwards_config.py
+++ legged_gym/legged_gym/envs/solo12/solo12_v3_1_upwards_config.py
@@ -1,6 +1,6 @@
 """First-stage upward-jump training configuration for Solo12 v3_1."""
 
-from legged_gym.envs.go1.go1_upwards_config import Go1UpwardsCfg, Go1UpwardsCfgPPO
+from legged_gym.envs.go2.go2_upwards_config import Go2UpwardsCfg, Go2UpwardsCfgPPO
 from legged_gym.envs.solo12.solo12_v3_1_common import (
     Solo12V31Asset,
     Solo12V31Control,
@@ -10,7 +10,7 @@ from legged_gym.envs.solo12.solo12_v3_1_common import (
 )
 
 
-class Solo12V31UpwardsCfg(Go1UpwardsCfg):
+class Solo12V31UpwardsCfg(Go2UpwardsCfg):
     task_name = "solo12_v3_1_upwards"
     init_state = Solo12V31InitState
     morphology = Solo12V31Morphology
@@ -18,18 +18,12 @@ class Solo12V31UpwardsCfg(Go1UpwardsCfg):
     asset = Solo12V31Asset
     viewer = Solo12V31Viewer
 
-    class env(Go1UpwardsCfg.env):
+    class env(Go2UpwardsCfg.env):
         reset_height = 0.12
 
-    class domain_rand(Go1UpwardsCfg.domain_rand):
-        # First learn a clean take-off. Robustness randomisation can be widened
-        # again after the jumping skill has formed.
-        push_robots = False
-        pos_vel_random_prob = 0.4
-        has_jumped_random_prob = 0.4
-
-        class ranges(Go1UpwardsCfg.domain_rand.ranges):
-            # Scaled to the 4.398 kg Solo body rather than the Go1 body.
+    class domain_rand(Go2UpwardsCfg.domain_rand):
+        class ranges(Go2UpwardsCfg.domain_rand.ranges):
+            # Scaled to the lighter Solo12 model rather than the Go2 model.
             motor_strength_ranges = [0.95, 1.05]
             p_gains_range = [0.95, 1.05]
             d_gains_range = [0.95, 1.05]
@@ -38,16 +32,10 @@ class Solo12V31UpwardsCfg(Go1UpwardsCfg):
             com_displacement_range = [-0.02, 0.02]
             added_link_mass_range = [0.9, 1.1]
 
-    class rewards(Go1UpwardsCfg.rewards):
+    class rewards(Go2UpwardsCfg.rewards):
         max_contact_force = 120.0
 
-        class scales(Go1UpwardsCfg.rewards.scales):
-            jumping = 100.0
-            task_max_height = 3000.0
-            action_rate = -0.1
-            energy_usage_actuators = -0.005
 
-
-class Solo12V31UpwardsCfgPPO(Go1UpwardsCfgPPO):
-    class runner(Go1UpwardsCfgPPO.runner):
+class Solo12V31UpwardsCfgPPO(Go2UpwardsCfgPPO):
+    class runner(Go2UpwardsCfgPPO.runner):
         experiment_name = "test_solo12_v3_1"
